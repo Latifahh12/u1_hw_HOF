8c160b0fb4452547e310ecf91796ae81cc03b87b
Latifah Hutchings <latifahbusprofile@gmail.com>
2022-07-01 00:19:07 -0600

    hard Homework

--- solution.js
+++ solution.js
@@ -1,6 +1,13 @@
 // You must RETURN your result in each function!
-
 function printNums(nums) {
+  const printNums = [1, 2, 3, 4]
+
+  printNums.forEach((nums) => {
+    console.log(nums)
+    return printNums
+  })
+
+  // }
   /**
    *  Using .forEach, add each number in the nums array to a new array.
    * @param nums => [1,2,3,4]
@@ -8,6 +15,12 @@ function printNums(nums) {
 }
 
 function returnSums(nums) {
+  const returnSums = [1, 2, 3, 4, 5]
+
+  returnSums.forEach(nums(n + index) => {
+     console.log(n + index)
+  })
+
   /**
    * Using forEach, push the sum of n plus the index
    * to a new array and return the new array
@@ -16,14 +29,19 @@ function returnSums(nums) {
 }
 
 function returnTotal(objs) {
-  /**
-   * Using forEach return a number that is the total sum of all numbers in the array of objects.
-   * The key for each object will be n
-   * @param objs => [{n:1}]
-   */
-}
+/**
+ * Using forEach return a number that is the total sum of all numbers in the array of objects.
+ * The key for each object will be n
+ * @param objs => [{n:1}]
+ */
 
 function printMoney(decimals) {
+
+  const decimals= [2.30, 2.40, 32.99]
+
+const newDecimalsMoney = money.map(($) => {
+    return newDecimalsMoney
+  })
   /**
    * @param decimals => [2.30, 2.40, 32.99]
    * Using .map, return an array of decimals formatted as dollars.
